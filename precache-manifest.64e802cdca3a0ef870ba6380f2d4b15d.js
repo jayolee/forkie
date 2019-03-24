6f5d14b45ce9873838e2370596668a155c968b95
jayolee <jayoungl@andrew.cmu.edu>
2019-03-24 00:48:22 -0400

    Updates

---
+++ precache-manifest.64e802cdca3a0ef870ba6380f2d4b15d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "706c637e5ae6c69828690a1e951cb3f5",
+    "url": "/forkie/static/media/recipe.706c637e.svg"
+  },
+  {
+    "revision": "d4b3125d0744469baa37",
+    "url": "/forkie/static/css/main.59727956.chunk.css"
+  },
+  {
+    "revision": "4ae04c6c8a93822d2862",
+    "url": "/forkie/static/js/runtime~main.4651f496.js"
+  },
+  {
+    "revision": "3e93cd349298d33a3386",
+    "url": "/forkie/static/js/2.227cd22b.chunk.js"
+  },
+  {
+    "revision": "48fedadbba01447b3f0e791fcdcafe8f",
+    "url": "/forkie/static/media/fork.48fedadb.svg"
+  },
+  {
+    "revision": "5f1ff5d9450433e8d8852d6ca74e6e68",
+    "url": "/forkie/static/media/home.5f1ff5d9.svg"
+  },
+  {
+    "revision": "1df2524f1d6d65509af46b2d1be045c5",
+    "url": "/forkie/static/media/messages.1df2524f.svg"
+  },
+  {
+    "revision": "d4b3125d0744469baa37",
+    "url": "/forkie/static/js/main.3561bb4f.chunk.js"
+  },
+  {
+    "revision": "6e46c4bee7a1b171b2d12d0aad5e7944",
+    "url": "/forkie/static/media/profile.6e46c4be.svg"
+  },
+  {
+    "revision": "bdc127bc3f7fd6d61484d4c4b519c8fc",
+    "url": "/forkie/static/media/home_on.bdc127bc.svg"
+  },
+  {
+    "revision": "c7e0802beeda4d2f24e48271e6c4c384",
+    "url": "/forkie/static/media/messages_on.c7e0802b.svg"
+  },
+  {
+    "revision": "433ec6c7f0f81c6cdfe12cd6316bcb47",
+    "url": "/forkie/static/media/recipe_on.433ec6c7.svg"
+  },
+  {
+    "revision": "b0609d39630d7cfa6e53516fea604497",
+    "url": "/forkie/static/media/profile_on.b0609d39.svg"
+  },
+  {
+    "revision": "6742aa1be0e312ce0e0a3fcdaf89ae80",
+    "url": "/forkie/index.html"
+  }
+];
